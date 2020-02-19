9014e674eb0b224147d157f2fa5eb8a09e65cbc0
Cameron Melville <cam.t.melville@gmail.com>
2020-02-19 23:21:28 +1100

    home page styled with links- links pages need completion

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3,
+  url: 'https://i.redd.it/31t84eqfv3611.jpg'
+)
+
+Flat.create!(
+  name: 'Our House.  ............................   . .Yo',
+  address: 'In the middle of our street .....  ........ 213',
+  description: 'A garden flat. No double bedrooms, no plan living area, no kitchen and a beautiful conservatory',
+  price_per_night: 100,
+  number_of_guests: 7,
+  url: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/camping-quotes-1556677391.jpg?crop=1.00xw:0.855xh;0,0.0384xh&resize=980:*'
+)
